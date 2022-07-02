32231a5002fda0dd7dcdaba82238b5bdd85cd977
WilliIB <willi.imgruth@gmail.com>
2022-07-02 22:33:56 +0200

    Adapt code

--- model-viewer.js
+++ model-viewer.js
@@ -1,3 +1,17 @@
+// import { projects } from "./projects.js";
+
+// // Get the current project ID from the URL parameter
+// const currentUrl = window.location.href; 
+// const url = new URL(currentUrl);
+// const currentProjectID = url.searchParams.get("id");
+
+// // Get the current project
+// const currentProject = projects.find(project => project.id === currentProjectID);
+
+// // Add the project URL to the iframe
+// const iframe = document.getElementById('model-iframe');
+// iframe.src = currentProject.url;
+
 import { projects } from "./projects.js";
 
 // Get the current project ID from the URL parameter
@@ -8,6 +22,18 @@ const currentProjectID = url.searchParams.get("id");
 // Get the current project
 const currentProject = projects.find(project => project.id === currentProjectID);
 
-// Add the project URL to the iframe
-const iframe = document.getElementById('model-iframe');
-iframe.src = currentProject.url;
+import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
+
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+async function loadIfc(url) {
+    await viewer.IFC.setWasmPath("./");
+    const model = await viewer.IFC.loadIfcUrl(url);
+    viewer.shadowDropper.renderShadow(model.modelID);
+}
+
+loadIfc(`static/IFC/${currentProject.ifc}`);
--- projects.js
+++ projects.js
@@ -1,52 +1,27 @@
 export const projects = [
     {
-        name: "Model 1 A",
+        name: "Model 1",
         id: "1",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/01/"
+        ifc: "01.ifc"
     },
     {
         name: "Model 2",
         id: "2",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/02/"
+        ifc: "02.ifc"
     },
     {
         name: "Model 3",
         id: "3",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/03/"
+        ifc: "03.ifc"
     },
     {
         name: "Model 4",
         id: "4",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/04/"
+        ifc: "04.ifc"
     },
     {
         name: "Model 5",
         id: "5",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05/"
+        ifc: "05.ifc"
     },
-    {
-        name: "Model 1 A",
-        id: "1",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/01/"
-    },
-    {
-        name: "Model 2",
-        id: "2",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/02/"
-    },
-    {
-        name: "Model 3",
-        id: "3",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/03/"
-    },
-    {
-        name: "Model 4",
-        id: "4",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/04/"
-    },
-    {
-        name: "Model 5",
-        id: "5",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05/"
-    }
 ]
